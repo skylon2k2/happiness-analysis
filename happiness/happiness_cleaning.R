1e3b1a3e9b09b7b2617fe00a631f7d64a4e363af
Mustafa Ali <90818966+skylon2k2@users.noreply.github.com>
2024-11-28 10:00:27 -0500

    Update happiness_cleaning.R
    change csv file name

--- happiness/happiness_cleaning.R
+++ happiness/happiness_cleaning.R
@@ -3,11 +3,10 @@ install.packages("fastDummies")
 library(fastDummies)
 
 #Import the csv file and display the first few rows
-HAPPY <- read.csv("export.csv")
+HAPPY <- read.csv("2019_happiness_cleaned.csv")
 head(HAPPY)
 
-#Create dummy variables
+#Create dummy variables, and remove "Australia & New Zealand" column
 HAPPY <- dummy_cols(HAPPY, select_columns = "region")
-head(HAPPY)
 HAPPY <- HAPPY[ -c(10) ]
 head(HAPPY)
